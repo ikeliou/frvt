cf3c8ed8853a215289d0fc412beb4c82feca5332
root <root@localhost.localdomain>
2019-10-15 06:04:40 -0400

    add compile isap5 script.

---
+++ 11/scripts/build_isap_impl5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root=$(pwd)
+
+echo "Attempting to build null implementation" 
+cd src/isapmpl5
+#make test.so
+#mv test.so ../../lib/libfrvt_11_isap_001.so
+rm -rf build; mkdir -p build; cd build
+#cmake ../ dev/null; make
+cmake ../ ; make
+
+cd $root
